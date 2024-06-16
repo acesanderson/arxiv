b557d0372f14a589a8897813076d486992c77264
Brian Anderson PC <brian.briananderson@gmail.com>
2024-06-15 23:10:52 -0500

    added argparse so its a proper cl tool, added notes for upcoming functionality

--- query_papers.py
+++ query_papers.py
@@ -1,25 +1,51 @@
+"""
+Command line utility that queries the chroma db for papers related to a given query.
+The embeddings are the abstracts.
+Upcoming functionality:
+- return Paper objects so you can see the authors, categories, etc.
+- a function that downloads the paper from arxiv.
+- fine-tuning a language model on 90,000+ papers (at 3 seconds a paper, that's 75 hours and ~2 terabytes of data).
+"""
+
 from arxiv import Paper
 import chromadb
-import sys
+import random
+import argparse
+
+queries = """
+Prompt engineering for agents
+Multi-agent reinforcement learning
+Evaluation of LLM responses
+Quality rubrics for evaluating LLM responses
+RAPTOR
+Metaprompting
+Flow Engineering
+""".strip().split("\n")
 
 # create chroma db
 client = chromadb.PersistentClient(path="vectordbs/arxiv")
 collection = client.get_collection("AI_papers_6_15_2024")
 
-def query_db(query):
+def query_db(query, k = 10):
     """
     Queries the chroma db for the given query.
     """
     results = collection.query(
-        query_texts=["This is a query document about hawaii"], # Chroma will embed this for you
-        n_results=10 # how many results to return
+        query_texts=[query], # Chroma will embed this for you
+        n_results=k # how many results to return
     )
-    return results    
+    return results
 
 if __name__ == '__main__':
-    query = "Metaprompting"
-    if len(sys.argv) > 1:
-        query = sys.argv[1]
-    results = query_db("Metaprompting")
-    titles = [result.split("::")[0] for result in results]
-    print(titles)
+    # Set query to a random query if no query is provided
+    query = random.choice(queries)
+    # argparse makes this so simple
+    parser = argparse.ArgumentParser(description="Example CLI tool using argparse.")
+    parser.add_argument('query', type=str, nargs = "?", default=query, help='The search query')
+    parser.add_argument('--k', type=int, default=10, help='Number of results to return')
+    args = parser.parse_args()
+    results = query_db(args.query, k=args.k)
+    # Recall that our ids are in the format arxiv_id::title
+    titles = [result.split("::")[1] for result in results['ids'][0]]
+    print(f"===============================================\nResults for query: {args.query}\n===============================================")
+    print("\n".join(titles))
