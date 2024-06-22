0991d5028981ca1d336e87e2f0299905b3650299
Brian Anderson PC <brian.briananderson@gmail.com>
2024-06-21 19:59:08 -0500

    loading 90k records into tinydb; put old json initialization code for the big kaggle dataset into refresh_database.py

--- metadata.py
+++ metadata.py
@@ -2,20 +2,17 @@ import pandas as pd
 from arxiv import Paper
 from tinydb import TinyDB, Query
 
-# Read the entire JSON file (only feasible if you have enough RAM or if you do this in chunks)
-# df = pd.read_json('arxiv-metadata-oai-snapshot.json', lines=True)
-# df.to_parquet('arxiv-metadata.parquet')
-# df = pd.read_parquet('arxiv-metadata.parquet')
-# ai = df[df['categories'].str.contains('cs.AI', na=False)]
-# # Save the AI papers to a new parquet file
-# ai.to_json('arxiv-metadata-ai.json', orient='records', lines=True)
-
-# load the ai papers
+# load our dataset from the big json file -- this is legacy and will be removed once we are comfortable with our tinydb database.
 ai = pd.read_json('arxiv-metadata-ai.json', lines=True)
+# load our tinydb database
+db = TinyDB('databases/tinydb/papers.json')
+# in case user needs a handy list of fields (accessed through metadata.fields)
+fields = list(Paper.__dataclass_fields__.keys())
 
+# load the arxiv metadata from the big json file
 def get_paper(row):
     """
-    Converts a row in our DataFrame to a Paper object.
+    Legacy function; only useful in converting json into Paper objects.
     """
     return Paper(
         title=row['title'],
@@ -36,13 +33,22 @@ def get_paper(row):
 
 def load_metadata() -> list:
     """
-    Load the metadata from the database.
+    Legacy function. Load the metadata from the big json file.
+    Use the TinyDB database instead.
     """
     return [get_paper(row) for index, row in ai.iterrows()]
 
+def query_metadata(query: str, field: str = "title", no_results = None) -> list:
+    """
+    Query the metadata using a search term.
+    To get a list of fields, access metadata.fields (it's a list).
+    """
+    User = Query()
+    results = db.search(User[field] == query)
+    return results[:no_results]
+
 papers = load_metadata()
 
-db = TinyDB('databases/tinydb/db.json')
 
 def insert_object(obj):
     """ Insert a Python object into the database """
@@ -57,3 +63,10 @@ def search_object(name):
 insert_object({'name': 'John Doe', 'age': 30})  # Example object
 result = search_object('John Doe')
 print(result)
+
+for index, paper in enumerate(papers):
+    print(f"Inserting paper {index + 1} of {len(papers)}")
+    insert_object(paper.__dict__)
+
+
+# ai = pd.read_json('arxiv-metadata-ai.json', lines=True)
